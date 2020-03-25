9bd9c778c52c8f339ae1981e899da9550a2b3cac
Beret <c445352742@126.com>
2020-03-26 00:41:42 +0800

    promise

--- index.js
+++ index.js
@@ -18,22 +18,35 @@ const P = require('./p.js')
 
 
 let p2 = new P(function (resolve, reject) {
-  console.log('start')
-  resolve('then1');
-},1)
-p2.then((e) => {
+  setTimeout(() => {
+    console.log('start')
+    resolve('then1');
+  }, 100)
+}, 1)
+p2
+.then((e) => {
   console.log(e)
   return new P(function (resolve, reject) {
-    resolve('then2')
-  },2)
+    setTimeout(() => {
+      // 11
+      resolve('then2')
+    }, 100)
+  }, 2)
 })
 .then((e) => {
+  // 22
   console.log(e)
   return 'then3'
 })
 .then((e) => {
-  console.log(e)
+  setTimeout(() => {
+    // 33
+    console.log(e)
+  }, 100)
 })
 .then((e) => {
-  console.log(e)
+  setTimeout(() => {
+    // 44
+    console.log(e)
+  }, 100)
 })
--- p.js
+++ p.js
@@ -3,6 +3,24 @@ P = function (fnc, v) {
   self.stat = 'pending'
   self.v = v || 0
   self.value = ''
+
+  self = new Proxy(self, {
+    set(target, key, value) {
+      target[key] = value
+      if (key == 'stat') {
+        for (let i = 0; i < self.resolvelist.length; i++) {
+          let temp = self.resolvelist[0].call(self, self.value);
+          self.resolvelist.splice(0, 1);
+          i--
+          if (temp instanceof P) {
+            break;
+          }
+          else { self.value = temp }
+        }
+      }
+    }
+  })
+
   self.resolve = function (e) {
     if (self.stat !== 'pending')
       return;
@@ -18,13 +36,19 @@ P = function (fnc, v) {
 
 P.prototype.then = function (e) {
   let self = this;
-  let ret = e(self.value)
-  if (ret instanceof P) {
-    return ret;
+  if (self.stat == 'pending') {
+    self.resolvelist.push(e)
+  } else {
+    let ret = e(self.value)
+    if (ret instanceof P) {
+      return ret;
+    }
+    self.value = ret || undefined
   }
-  self.value = ret || undefined
   return self;
 }
+
+P.prototype.resolvelist = []
 
 
 
