7dbe7602d96a12178bff1390c48cd86f96ee0838
Beret <c445352742@126.com>
2020-08-10 14:12:17 +0800

    to projects

---
+++ Backend/web.go
@@ -0,0 +1,15 @@
+package main
+import (
+				// "net/http"
+				"fmt"
+)
+
+func main() {
+        // http.Handle("/", http.FileServer(http.Dir("http.Dir('/home/chai/src')))")))
+				// http.ListenAndServe(":80", nil)
+				res,err=cal()
+				fmt.Println(a)
+}
+func cal() Error() String{
+	return 0,Error() String
+}
